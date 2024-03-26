0fdb41378c1f792212aa854a7767d9103023d1a8
unknown <bsc-08-18@unima.ac.mw>
2024-03-26 22:49:21 +0200

    changed the menu-item component to navigate to the different items using the useNavigate library from react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import './App.css';
-
 import HomePage from './pages/homepage/HomePage';
+import { Route, Routes } from 'react-router-dom';
+
+const Hats = () => (
+  <div>
+    <h1>Hats page</h1>
+  </div>
+);
+
+
 function App() {
   return (
-    <div className="App">
-     <HomePage/>
-    </div>
+    
+    <Routes>
+      <Route  path="/" element={<HomePage />} />
+      <Route path="/hats" element={<Hats />} />
+    </Routes>
   );
 }
 
--- src/components/menu-item/MenuItem.js
+++ src/components/menu-item/MenuItem.js
@@ -1,21 +1,28 @@
 import React from 'react';
-import './MenuItem.css'
+import './MenuItem.css';
+import { useNavigate} from 'react-router-dom'; // Importing the hooks from react-router-dom
 
-const MenuItem = ({title, imageUrl, size}) => {
-    return (
-            <div className={`${size} menu-item`} >
-                <div
-                className='background-image'
-                style={{
-                    backgroundImage: `url(${imageUrl})`
-                }}
-                />
-                    <div className='content'>
-                        <h1 className='title'>{title.toUpperCase()}</h1>
-                        <span className='subtitle'>$SHOP NOW</span>
-                    </div>
-            </div>
-    );
-}
+const MenuItem = ({ title, imageUrl, size, linkUrl }) => {
+  const navigate = useNavigate(); // Getting the navigation function
+
+  const handleClick = () => {
+    navigate(`/${linkUrl}`);
+  };
+
+  return (
+    <div className={`${size} menu-item`} onClick={handleClick}>
+      <div
+        className='background-image'
+        style={{
+          backgroundImage: `url(${imageUrl})`
+        }}
+      />
+      <div className='content'>
+        <h1 className='title'>{title.toUpperCase()}</h1>
+        <span className='subtitle'>SHOP NOW</span>
+      </div>
+    </div>
+  );
+};
 
 export default MenuItem;
